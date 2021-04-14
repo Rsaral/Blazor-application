1183f6d5bc57f7dd4c7fbd368652a45300951cbb
DESKTOP-ATOP5G7\Rünno <rynno.saral@gmail.com>
2021-04-14 10:18:59 +0300

    Töötajate nimede, andmete ja piltide laadimine

---
+++ BlazorTutorial/EmployeeManagement.Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class Class1
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+    }
+}
